50b4ba1fed89b3fc0b7158604bf17b1ee8bb359c
alufurn7 <alufurn7@gmail.com>
2026-05-01 15:15:58 +0530

    Update googleSheets.ts

--- lib/googleSheets.ts
+++ lib/googleSheets.ts
@@ -1,11 +1,10 @@
 import { google } from "googleapis";
 
-const auth = new google.auth.JWT(
-  process.env.GOOGLE_CLIENT_EMAIL,
-  null,
-  process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-  ["https://www.googleapis.com/auth/spreadsheets"]
-);
+const auth = new google.auth.JWT({
+  email: process.env.GOOGLE_CLIENT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+});
 
 export async function addToSheet(data: any) {
   const sheets = google.sheets({ version: "v4", auth });
